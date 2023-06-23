ad7355c73861141c55d7e1af4755369eb86e6255
Alexey Sevastynov <alexseva94@gmail.com>
2023-06-23 17:09:24 +0300

    fill post from backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,6 +17,7 @@ export const Home = () => {
   const { posts, tags } = useSelector((state) => state.posts);
 
   const isPostLoading = posts.status === "loading";
+  const isTagsLoading = posts.status === "loading";
   React.useEffect(() => {
     dispatch(fetchPosts());
     dispatch(fetchTags());
@@ -41,7 +42,7 @@ export const Home = () => {
               <Post key={index} isLoading={true}></Post>
             ) : (
               <Post
-                id={obj._id}
+                _id={obj._id}
                 title={obj.title}
                 imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
                 user={{
@@ -60,10 +61,7 @@ export const Home = () => {
           )}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock
-            items={["react", "typescript", "заметки"]}
-            isLoading={false}
-          />
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
           <CommentsBlock
             items={[
               {
